dc0869aaecc8a5b2d335fdb1eba91a97bc03807c
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-08 13:58:55 -0800

    add if statement in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 function convertNumber(userNumber){
   var numberCount = [];
+  var word = ["Beep!","Boop!","I'm sorry, Dave. I'm afraid I can't do that."] // word[0]= Beep , word[1]= Boop , word[2] = "I'm sorry"
   for (var index = 0; index <= userNumber; index ++){
-    numberCount.push(index);
+    if(index === 0){
+      index = "Beep!"
+      numberCount.push(index);
+      debugger
+    }else{
+      return index;
+    }
+    //numberCount.push(index);
   }
   return numberCount;
 }
@@ -13,6 +21,6 @@ $(document).ready(function(){
     event.preventDefault();
     var userNumber = parseInt($("input#numberInput").val());
     var result = convertNumber(userNumber);
-    $("#result").text(result).show(); 
+    $("#result").text(result).show();
   });
 });
