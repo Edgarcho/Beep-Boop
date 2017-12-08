4a70b6565c921b25924db8689c15e653410a9852
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-08 14:17:19 -0800

    pass the second specifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 function convertNumber(userNumber){
   var numberCount = [];
-  var word = ["Beep!","Boop!","I'm sorry, Dave. I'm afraid I can't do that."] // word[0]= Beep , word[1]= Boop , word[2] = "I'm sorry"
+  //var word = ["Beep!","Boop!","I'm sorry, Dave. I'm afraid I can't do that."] // word[0]= Beep , word[1]= Boop , word[2] = "I'm sorry"
   for (var index = 0; index <= userNumber; index ++){
+    debugger
     if(index === 0){
-      index = "Beep!"
+      index = "Beep!";
       numberCount.push(index);
+      index = 0;
+      //debugger
+    }else if(index === 1){
+      index = "Boop!";
+      numberCount.push(index);
+      index = 1;
+    }else if(index % 3 === 0){
+      index = "I'm sorry, Dave. I'm afraid I can't do that.";
+      numberCount.push(index);
+      index = 3;
       debugger
-    }else{
-      return index;
+    }
+    else{
+      numberCount.push(index);
     }
     //numberCount.push(index);
   }
