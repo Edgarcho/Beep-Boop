1e71cf14f6c9a3ebfb1e84358113b0b4b7c909e7
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-08 12:00:20 -0800

    add for loop in function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 function convertNumber(userNumber){
- alert(userNumber);
-};
+  var numberCount = [];
+  for (var index = 0; index <= userNumber; index ++){
+    numberCount.push(index);
+  }
+  return numberCount;
+}
 
 
 // user interface logic
@@ -9,6 +13,6 @@ $(document).ready(function(){
     event.preventDefault();
     var userNumber = parseInt($("input#numberInput").val()); //convert the string to a number
     var result = convertNumber(userNumber); // runs the function convertNumber with the value of the user and returns the value as result.
-    $("#result").text(result); //return the result in text
+    $("#result").text(result).show(); //return the result in text
   });
 });
