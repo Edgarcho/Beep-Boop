8d7c43d3661d863f0afeec6650b2aebcb1388858
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-08 16:44:58 -0800

    add background image in CSS

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,11 @@ function convertNumber(userNumber){
   for (var index = 0; index <= userNumber; index ++){
     debugger
     if(index === 0){
-      //index = "Beep!";
       numberCount.push("Beep!");
-      //index = 0;
-      debugger
     }else if(index === 1){
-      //index = "Boop!";
       numberCount.push("Boop!");
-      //index = 1;
     }else if(index % 3 === 0){
-      //index = "I'm sorry, Dave. I'm afraid I can't do that.";
       numberCount.push("I'm sorry, Dave. I'm afraid I can't do that.");
-      //index = 3;
-      debugger
     }
     else{
       numberCount.push(index);
